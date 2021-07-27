efbd475a75a943a3ce330f9e7ea6431ae40a1e3b
Duy Nguyen <Zemnas768@gmail.com>
2021-07-27 17:42:29 -0400

    Added additional info to about me

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -21,18 +21,21 @@ function Home() {
                     <div className="row">
                         <div className="col-md-12">
                             <a href="https://github.com/LinosM">
-                                <img src={gitImg} alt="github" width="40" height="40" />        
+                                <img src={gitImg} alt="github" width="40" height="40" />
                             </a>
                             <a href="https://www.linkedin.com/in/duy-nguyen-b979b01b7/">
                                 <img src={inImg} alt="linkedin" width="40" height="40" />
                             </a>
                         </div>
                     </div>
-                    
-                    <hr/>
+
+                    <hr />
                     <img className="rounded float-left mr-4 img-fluid" src={profilePic} alt="placeholder" width="200" height="200" />
                     <p>
-                        I am a Full-Stack web developer with a certificate from The Coding Boot Camp at UNC-Chapel Hill.
+                        A self-motivated full-stack web developer with primarily back-end experience but some front-end as well. Recently earned a certificate in full-stack development from The Coding Boot Camp at UNC-Chapel Hill, with newly developed skills in JavaScript, HTML, CSS, React.js, and responsive web design, as well as strong skills and ability in writing clean and efficient code. 
+                    </p>
+                    <p>
+                        With each project, the goal is to improve upon previous ones to ensure a better engaging experience for the end-user. On a recent project, I worked with a team of four to develop a MERN app for users and teams to manage and track various projects and tasks with over 170 personal commits within a month. I’m hoping to utilize my skill set with a future team to build better experiences on the web.
                     </p>
                     <p>
                         I specialize in React, Node.js, Express.js, MongoDB, jQuery, Bootstrap, & Bulma. I also have experience with HTML, CSS, Heroku, MySQL, APIs, JSON, AJAX.
