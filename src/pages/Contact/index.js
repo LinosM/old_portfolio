5991309dcc9dca0856894c8e667ba606709ee398
Duy Nguyen <Zemnas768@gmail.com>
2021-07-27 17:42:14 -0400

    Removed br line

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -11,7 +11,6 @@ function Contact() {
                 <h1>Contact</h1>
                 <hr />
                 <p>If you wish to contact me, you can reach me via email or the following links below:</p>
-                <br />
                 <p><a href="mailto:NguyenDuy768@gmail.com">NguyenDuy768@gmail.com</a></p>
                 <p>GitHub: <a href="https://github.com/LinosM">https://github.com/LinosM</a></p>
                 <p>LinkedIn: <a href="https://www.linkedin.com/in/duy-nguyen-b979b01b7/">https://www.linkedin.com/in/duy-nguyen-b979b01b7/</a></p>
