6898200e0470a32c0c273b1e2625021a9ee717b1
Duy Nguyen <Zemnas768@gmail.com>
2021-07-27 19:05:23 -0400

    Added a section in the Portfolio page to display information about each project highlighted

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -6,11 +6,21 @@ import portJson from "../../portfolio.json";
 
 
 function Portfolio() {
-    const [portfolio, setPortfolio] = useState([])
-    
+    const [portfolio, setPortfolio] = useState([
+        {
+            about: ""
+        }
+    ]);
+    const [value, setValue] = useState(0);
+
     useEffect(() => {
         setPortfolio(portJson)
     }, []);
+
+    function onChange(value) {
+        setValue(value);
+        console.log(value);
+    }
 
     return (
         <div>
@@ -18,23 +28,45 @@ function Portfolio() {
                 <title>Portfolio | Duy Nguyen</title>
             </Helmet>
             <div className="col-md-12 mt-5">
-                <section style={{maxHeight: "700px", maxWidth: "800px"}}>
-                    <h1>Portfolio</h1>
-                    <hr />
-                    <div className="row">
-                        <Carousel>
-                            {portfolio.map(port => (
-                                <Card 
-                                    key={port.id}
-                                    img={port.img}
-                                    name={port.name}
-                                    demo={port.demo}
-                                    git={port.git}
-                                />
-                            ))}
-                        </Carousel>
+                <div className="row">
+                    <div className="col-md-8">
+                        <section style={{ maxHeight: "900px", maxWidth: "800px" }}>
+                            <h1>Portfolio</h1>
+                            <hr />
+                            <div className="row">
+                                <Carousel
+                                    slideIndex={value}
+                                    afterSlide={onChange}
+                                >
+                                    {portfolio.map(port => (
+                                        <Card
+                                            key={port.id}
+                                            img={port.img}
+                                            name={port.name}
+                                            demo={port.demo}
+                                            git={port.git}
+                                        />
+                                    ))}
+                                </Carousel>
+                            </div>
+                        </section>
                     </div>
-                </section>
+                    <div className="col-md-4">
+                        <section>
+                            <h1>About</h1>
+                            <hr />
+                            {portJson[value].about}
+                            <h3 className="mt-3">Technology Used</h3>
+                            <hr />
+                            
+                            <ul>
+                                {portJson[value].tech.map(tech => (
+                                    <li>{tech}</li>
+                                ))}
+                            </ul>
+                        </section>
+                    </div>
+                </div>
             </div>
         </div>
     )
